9b2588d63816434e4902cbd0ec9932b1c617f248
Luca Zhovtovsky <lukazhovtovski@gmail.com>
2023-02-16 20:27:47 +0700

    init config

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"github.com/lumorow/todo-app"
 	"github.com/lumorow/todo-app/pkg/handler"
+	"github.com/lumorow/todo-app/pkg/repository"
+	"github.com/lumorow/todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
 	}
+}
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigFile("config")
+	return viper.ReadInConfig()
 }
