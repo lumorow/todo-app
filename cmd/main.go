d742f292627aa200e4e3a12b55d7e8a689b83b65
Luca Zhovtovsky <lukazhovtovski@gmail.com>
2023-02-17 00:24:49 +0700

    viper problems with read file

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
 	"github.com/lumorow/todo-app"
 	"github.com/lumorow/todo-app/pkg/handler"
 	"github.com/lumorow/todo-app/pkg/repository"
@@ -13,7 +14,20 @@ func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
@@ -23,7 +37,8 @@ func main() {
 	}
 }
 func initConfig() error {
-	viper.AddConfigPath("configs")
+	viper.AddConfigPath("./configs")
 	viper.SetConfigFile("config")
+	//viper.SetConfigType("yaml")
 	return viper.ReadInConfig()
 }
